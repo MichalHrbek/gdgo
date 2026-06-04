bb3005eb6fac20189ae14ff64810cf90fbd45eb3
Michal Hrbek <michal@dee.cz>
2026-06-04 19:26:33 +0200

    Checking for liberties

--- scripts/go/game_state.gd
+++ scripts/go/game_state.gd
@@ -11,13 +11,96 @@ var to_play: Stone.StoneColor = BLACK
 var comment := ""
 var move_number := 0
 
+var black_captured := 0 # Black stones that have been captured
+var white_captured := 0 # White stones that have been captured
+
 var arrows: Array[Arrow] = []
 var lines: Array[Line] = []
 
 func get_stone(pos: Vector2i) -> Stone:
+	if not is_valid_position(pos):
+		return null
 	if not pos in stones:
 		stones[pos] = Stone.new()
 	return stones[pos]
+
+func is_valid_position(pos: Vector2i) -> bool:
+	return pos.x > 0 and pos.y > 0 and pos.x <= board_size.x and pos.y <= board_size.y
+
+func neighbours(source: Vector2i) -> Array[Vector2i]:
+	return [
+	source+Vector2i(1,0),
+	source+Vector2i(-1,0),
+	source+Vector2i(0,1),
+	source+Vector2i(0,-1)]
+
+func ffil(source: Vector2i) -> Array[Vector2i]:
+	var source_color = get_stone(source).color
+	var group: Array[Vector2i] = []
+	var queue: Array[Vector2i] = [source]
+	while queue:
+		var s = queue.pop_front()
+		if not is_valid_position(s):
+			continue
+		if get_stone(s).color != source_color:
+			continue
+		if s in group:
+			continue
+		group.append(s)
+		queue.push_back(s+Vector2i(1,0))
+		queue.push_back(s+Vector2i(-1,0))
+		queue.push_back(s+Vector2i(0,1))
+		queue.push_back(s+Vector2i(0,-1))
+	return group
+
+func flib(source: Vector2i) -> Array[Array]:
+	var source_color = get_stone(source).color
+	var group: Array[Vector2i] = []
+	var queue: Array[Vector2i] = [source]
+	var liberties: Array[Vector2i] = []
+	while queue:
+		var s = queue.pop_front()
+		if not is_valid_position(s):
+			continue
+		var c := get_stone(s).color
+		if c != source_color:
+			if c == Stone.StoneColor.NONE:
+				if s not in liberties:
+					liberties.append(s)
+			continue
+		if s in group:
+			continue
+		group.append(s)
+		queue.push_back(s+Vector2i(1,0))
+		queue.push_back(s+Vector2i(-1,0))
+		queue.push_back(s+Vector2i(0,1))
+		queue.push_back(s+Vector2i(0,-1))
+	return [group, liberties]
+
+func clear_if_dead(source: Vector2i) -> int:
+	var result = flib(source)
+	var group = result[0]
+	var liberties = result[1]
+	if not liberties:
+		for i in group:
+			get_stone(i).color = Stone.StoneColor.NONE
+		return len(group)
+	return 0
+
+func record_captured(color: Stone.StoneColor, ammount: int) -> void:
+	if color == Stone.StoneColor.WHITE: white_captured += ammount
+	elif color == Stone.StoneColor.BLACK: black_captured += ammount
+
+func after_move_check(move: Vector2i):
+	var c := get_stone(move).color
+	for i in neighbours(move):
+		var s := get_stone(i)
+		if not s:
+			continue
+		if s.color not in [Stone.StoneColor.NONE, c]:
+			record_captured(s.color, clear_if_dead(i))
+	
+	record_captured(c, clear_if_dead(move))
 
 static func from_sgf(node: SGF.SgfNode) -> GameState:
 	var s = GameState.new()
@@ -30,11 +113,13 @@ static func from_sgf(node: SGF.SgfNode) -> GameState:
 				"B":
 					assert(value is SgfTypes.SgfPoint)
 					s.get_stone(value.to_ivec()).color = BLACK
+					s.after_move_check(value.to_ivec())
 					s.to_play = WHITE
 					s.move_number += 1
 				"W":
 					assert(value is SgfTypes.SgfPoint)
 					s.get_stone(value.to_ivec()).color = Stone.StoneColor.WHITE
+					s.after_move_check(value.to_ivec())
 					s.to_play = BLACK
 					s.move_number += 1
 				"AB":
